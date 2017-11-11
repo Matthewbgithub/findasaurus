f9b138f6f774b338857b57788e150b28f9db237b
Matt <Matthewbarlow98@gmail.com>
2017-11-11 17:39:29 +0000

    generates dinos

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 $(document).ready(function(){
-    console.log("jquery loaded");
+ 	var x = 50;
+	var y = 50;
+	var loops = 500;
+	var maxDinos = 2;
+	var no = 0;
+	start();
+	
+	function start(){
+		$('.frameInner').append('<img class="dinoInFrame" style="display:none;" src="./DinoPics/dino0.png">');
+		for(var i = 0; i < loops; i++){
+			x = Math.random()*100;
+			y = Math.random()*100;
+			generateDinosaur(x,y,i);
+		}
+	}
+	function generateDinosaur(x,y,i){
+		if(i>no){
+			no++;
+		}
+		if(no > maxDinos){
+			no = 0;
+		}
+		$('.frameInner').append('<img style="left: calc( '+x+'% - '+($('.dinoInFrame').width())/2+'px );top: calc( '+y+'% - '+($('.dinoInFrame').height())/2+'px );" class="dinoInFrame" src="./DinoPics/dino'+no+'.png">');
+	}
 });
